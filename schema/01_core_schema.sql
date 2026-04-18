1f7f56f0c00efed827de6425a4cb2bb8ed7977e2
Daria Bondarchuk <dariabondarchuk.work@gmail.com>
2026-04-18 22:59:08 +0300

    Add reviews and product_attributes tables to schema. Add indexes for foreign keys

--- schema/01_core_schema.sql
+++ schema/01_core_schema.sql
@@ -23,6 +23,7 @@ create table if not exists products (
     category varchar(50) not null
 );
 
+
 drop table if exists orders cascade;
 create table if not exists orders (
     id serial primary key,
@@ -33,6 +34,7 @@ create table if not exists orders (
     updated_at timestamp not null
 );
 
+
 drop table if exists order_items cascade;
 create table if not exists order_items (
     id serial primary key,
@@ -41,3 +43,34 @@ create table if not exists order_items (
     price_at_purchase decimal(10, 2) not null,
     quantity integer not null check (quantity > 0)
 );
+
+
+drop table if exists reviews;
+create table if not exists reviews (
+    user_id integer not null references users(id) on delete cascade,
+    product_id integer not null references products(id) on delete cascade,
+    created_at timestamp not null,
+    content varchar(100) not null,
+    title varchar(50) not null,
+    rating integer not null check(rating >= 1 and rating <= 5),
+    primary key(user_id, product_id)
+);
+
+
+-- a bit redundant, created to experiment with jsonb
+-- is index created for product_id?
+drop table if exists product_attributes;
+create table if not exists product_attributes (
+    product_id integer primary key references products(id) on delete cascade,
+    attributes jsonb not null,
+    created_at timestamp not null,
+    updated_at timestamp not null
+);
+
+
+create index idx_orders_user_id on orders(user_id);
+create index idx_order_items_order_id on order_items(order_id);
+create index idx_order_items_product_id on order_items(product_id);
+--user_id is already indexed as a first key in composite 
+create index idx_reviews_product_id on reviews(product_id);
+
