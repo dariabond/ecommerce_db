6684cde10730202e44ca84824e2d5cef0fc18e72
Daria Bondarchuk <dariabondarchuk.work@gmail.com>
2026-04-24 15:57:18 +0300

    Add seed files for users and categories.

---
+++ schema/02_seed.sql
@@ -0,0 +1,55 @@
+--Insert into users
+insert into users(first_name, last_name, email, phone_num, created_at, birth_year, status)
+with names as (
+	select 
+		array['James', 'Mary', 'John', 'Patricia', 'Robert', 'Jennifer', 'Michael', 'Linda', 'William', 'Elizabeth',
+          'David', 'Barbara', 'Richard', 'Susan', 'Joseph', 'Jessica', 'Thomas', 'Sarah', 'Charles', 'Karen',
+          'Christopher', 'Nancy', 'Daniel', 'Lisa', 'Matthew', 'Betty', 'Anthony', 'Margaret', 'Donald', 'Sandra',
+          'Mark', 'Ashley', 'Paul', 'Dorothy', 'Steven', 'Kimberly', 'Andrew', 'Emily', 'Kenneth', 'Donna',
+          'Joshua', 'Michelle', 'Kevin', 'Carol', 'Brian', 'Amanda', 'George', 'Melissa', 'Edward', 'Deborah',
+          'Stephen', 'Anna', 'Larry', 'Brenda', 'Justin', 'Pamela', 'Scott', 'Nicole', 'Brandon', 'Emma',
+          'Benjamin', 'Samantha', 'Samuel', 'Katherine', 'Frank', 'Christine', 'Gregory', 'Debra', 'Raymond', 'Rachel',
+          'Alexander', 'Catherine', 'Patrick', 'Carolyn', 'Jack', 'Janet', 'Dennis', 'Ruth', 'Jerry', 'Maria'] as first_names,
+		array['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis', 'Rodriguez', 'Martinez',
+          'Hernandez', 'Lopez', 'Gonzalez', 'Wilson', 'Anderson', 'Thomas', 'Taylor', 'Moore', 'Jackson', 'Martin',
+          'Lee', 'Perez', 'Thompson', 'White', 'Harris', 'Sanchez', 'Clark', 'Ramirez', 'Lewis', 'Robinson',
+          'Walker', 'Young', 'Allen', 'King', 'Wright', 'Scott', 'Torres', 'Nguyen', 'Hill', 'Flores',
+          'Green', 'Adams', 'Nelson', 'Baker', 'Hall', 'Rivera', 'Campbell', 'Mitchell', 'Carter', 'Roberts',
+          'Watson', 'Brooks', 'Chavez', 'Wood', 'James', 'Bennett', 'Gray', 'Mendoza', 'Ruiz', 'Hughes',
+          'Price', 'Alvarez', 'Castillo', 'Sanders', 'Patel', 'Myers', 'Long', 'Ross', 'Foster', 'Jimenez'] as last_names
+)
+select 
+	fn as first_name,
+	ln as last_name,
+	lower(fn || '.' || ln) || '@gmail.com' as email,
+	'+3' || LPAD(floor(random() * 10000000000)::text, 11, '0') as phone_num,
+	current_timestamp as created_at,
+	floor(random() * (2010 - 1984 + 1) + 1984)::int as birth_year,
+	'user' as status
+from names
+cross join unnest(first_names) as fn
+cross join unnest(last_names) as ln
+order by random();
+
+--Insert into users
+insert into categories(name, description)
+with names as (
+     select array[
+        'Electronics',
+        'Clothing',
+        'Home & Kitchen',
+        'Sports',
+        'Books',
+        'Toys',
+        'Beauty',
+        'Automotive',
+        'Garden',
+        'Health'
+    ] as categories
+)
+select 
+     category as name, 
+     'category_' || idx as description
+from names 
+cross join unnest(categories) with ordinality as t(category, idx);
+
